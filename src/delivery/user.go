96d08e05c1e43b16b198796bf379fa409587d72d
rudikurniawan99 <rudikurniawan501@gmail.com>
2022-10-20 14:01:59 +0700

    chore(user): add handler for delete and getuser

--- src/delivery/user.go
+++ src/delivery/user.go
@@ -1,6 +1,8 @@
 package delivery
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rudikurniawan99/go-api-4/src/model"
 	"github.com/rudikurniawan99/go-api-4/src/response"
@@ -21,6 +23,8 @@ func NewUserDelivery(u usecase.UserUsecase) UserDelivery {
 
 func (d *userDelivery) Mount(group *gin.RouterGroup) {
 	group.GET("/", d.getAllUserHanlder)
+	group.GET("/:id", d.getUserHanlder)
+	group.DELETE("/:id", d.deleteUserByIdHandler)
 }
 
 func (d *userDelivery) getAllUserHanlder(c *gin.Context) {
@@ -32,4 +36,44 @@ func (d *userDelivery) getAllUserHanlder(c *gin.Context) {
 	}
 
 	response.JsonSuccess(c, 200, users)
+}
+
+func (d *userDelivery) getUserHanlder(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+
+	if err != nil {
+		response.JsonFailed(c, 500, err)
+		return
+	}
+
+	user := &model.User{}
+
+	if err := d.u.FindById(user, id); err != nil {
+		response.JsonNotFound(c, err)
+		return
+	}
+
+	response.JsonSuccess(c, 200, user)
+}
+
+func (d *userDelivery) deleteUserByIdHandler(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		response.JsonFailed(c, 500, err)
+		return
+	}
+
+	user := &model.User{}
+
+	if err := d.u.FindById(user, id); err != nil {
+		response.JsonNotFound(c, err)
+		return
+	}
+
+	if err := d.u.DeleteById(user, int(user.ID)); err != nil {
+		response.JsonFailed(c, 500, err)
+		return
+	}
+
+	response.JsonSuccessWithMessage(c, 200, "success delete user", []any{})
 }
